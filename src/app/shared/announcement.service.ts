445cb0e8bea15d9175a6bdfbea6662c9f3029a29
Taras <tarashevchuk@gmail.com>
2022-01-24 21:21:45 +0200

    inited project

---
+++ src/app/shared/announcement.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnnouncementService {
+
+  constructor() { }
+
+  addAnnouncement() {
+
+  }
+  updateAnnouncement() {
+
+  }
+  removeAnnouncement() {
+
+  }
+  getAllAnnouncement() {
+
+  }
+}
